1cd4049450ab40d0e55b1518d68e24d639e2f4f1
Igal Sokolovsky <igal.s.1994@gmail.com>
2021-05-05 12:48:22 +0300

    server prod path fixed

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ const dev = {
 };
 
 const prod = {
-	API_URL: 'https://frozen-atoll-55432.herokuapp.com/'
+	API_URL: 'https://frozen-atoll-55432.herokuapp.com'
 };
 
 export const config = process.env.NODE_ENV === 'development' ? dev : prod;
